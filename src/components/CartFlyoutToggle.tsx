405e6d93eaba62a7bbbc4dd5a85fd5fe1756c95c
kharioki <tony.kharioki@gmail.com>
2022-11-12 16:31:18 +0300

    jsx config and some comments

--- src/components/CartFlyoutToggle.tsx
+++ src/components/CartFlyoutToggle.tsx
@@ -2,6 +2,8 @@ import { useStore } from '@nanostores/preact';
 import { isCartOpen } from '../cartStore';
 
 export default function CartFlyoutToggle() {
+	// read the store value with the useStore hook
 	const $isCartOpen = useStore(isCartOpen);
+	// write to the imported store using .set()
 	return <button onClick={() => isCartOpen.set(!$isCartOpen)}>Cart</button>;
 }
